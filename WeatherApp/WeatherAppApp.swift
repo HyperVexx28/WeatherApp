8cfb6f5b93e19054d74eea02b0e870c95e9491e3
Mohamad <Mshehab28@hotmail.com>
2023-07-25 13:05:35 +0100

    Initial Commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Mohamad Shehab on 25/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
